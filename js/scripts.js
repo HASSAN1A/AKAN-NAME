0a0439c2210369695abcdf30d9bbb35e29951131
Hassan Juma <okothhassanjuma@gmail.com>
2020-08-22 20:25:05 +0300

    add scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
 // var x = document.getElementById("myDate");
-// var x = document.createElement("INPUT");
-// x.setAttribute("type", "date");
 
 // function validate(form) {
 //   var isChecked = Array.prototype.some.call(form.querySelectorAll('input[name=gender]'), function (radio) {
@@ -58,44 +56,32 @@
 //   var message = "user entered this value: " + a;
 
 //   document.getElementById("alert").innerHTML = a;
-// }
-function akanName() {
+
+
+function useInput() {
   var gender = document.getElementById("gender").value;
   var bdate = document.getElementById("bdate").value;
+
+  var akanMen = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+  var akanWomen = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+
+  var CC = parseInt(bdate.substr(0, 2))
+  var YY = parseInt(bdate.substr(1, 3))
+  var MM = parseInt(bdate.substr(5, 6))
+  var DD = parseInt(bdate.substr(8, 9))
+  var d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
+
+  // if (gender === "Male") {
+  //   alert("your" + akanMen[d])
+  // } else
+  //   alert("your" + akanWomen[d])
+
+
+  alert(gender)
+  alert(bdate)
 }
 
 
-  // function submittedData() {
-  //   var a = document.getElementById("birthday").value;
-  //   alert(a);
-  // }
-  // }
-  // if (first==="3") {
-  //   alert("male");
-  // } else if (first==="4") {
-  //   alert("female");
-  // } else {
-  //   alert("error");
-  // }
-
-
-  // var d = new Date();
-  // var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  // document.getElementById("demo").innerHTML = days[d.getDay()];
-
-
-  // var d = new Date();
-  // document.getElementById("demo").innerHTML = d.getDay();
-
-
-
-
-
-
-
-
-  // const date1 = new Date('August 19, 1975 23:15:30 GMT-11:00');
-  // console.log(date1.getUTCDay());
 
 
 
@@ -116,43 +102,44 @@ function akanName() {
 
 
 
-
-  //     var d = [Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday]
-  //     var gender = (male, female)
-
-  //     var gender = ""
+// var d = [sunday, monday, tuesday, wednesday, thursday, friday, saturday]
 
 
-  // if (d === sunday && gender === male) {
-  //   console.log(you were born on Sunday and your Akan name is Kwasi)
-  // } else if ( === monday && gender === male) {
-  //   console.log(you were born on Monday and your Akan name is Kwadwo)
-  // } else if (d === tuesday && gender === male) {
-  //   console.log(you were born on Tuesday and your Akan name is Kwabena)
-  // } else if (d === wednesday && gender === male) {
-  //   console.log(you were born on Wednesday and your Akan name is Kwaku)
-  // } else if ( === thursday && gender === male) {
-  //   console.log(you were born on Thursday and your Akan name is Yaw)
-  // } else if (d === friday && gender === male) {
-  //   console.log(you were born on Fridayday and your Akan name is Kofi)
-  // } else if (d === saturday && gender === male) {
-  //   console.log(you were born on Saturday and your Akan name is Kwame)
-  // } else {
-  //   console.log(Sorry dude!)
-  // }
-  // if (d === sunday && gender === female) {
-  //   console.log(you were born on Sunday and your Akan name is Akosua)
-  // } else if (d === monday && gender === female) {
-  //   console.log(you were born on Monday and your Akan name is Adwoa)
-  // } else if (d === tuesday && gender === female) {
-  //   console.log(you were born on Tuesday and your Akan name is Abenaa)
-  // } else if (d === wednesday && gender === female) {
-  //   console.log(you were born on Wednesday and your Akan name is Akua)
-  // } else if (d === thursday && gender === female) {
-  //   console.log(you were born on Thursday and your Akan name is Yaa)
-  // } else if (d === friday && gender === female) {
-  //   console.log(you were born on Friday and your Akan name is Afua)
-  // } else if ( === saturday && gender === female) {
-  //   console.log(you were born on Saturday and your Akan name is Ama)
-  // } else {
-  //   console.log(Sorry!Lady.)
+
+
+
+
+
+// if (d === sunday && gender === male){
+//   alert("you were born on Sunday and your Akan name is " + Kwasi)
+// } else if (d === monday && gender === male) {
+//   alert("you were born on Monday and your Akan name is " + Kwadwo)
+// } else if (d === tuesday && gender === male) {
+//   console.log("you were born on Tuesday and your Akan name is "+ Kwabena)
+// } else if (d === wednesday && gender === male) {
+//   console.log("you were born on Wednesday and your Akan name is" Kwaku)
+// } else if (d === thursday && gender === male) {
+//   console.log("you were born on Thursday and your Akan name is "Yaw)
+// } else if (d === friday && gender === male) {
+//   console.log("you were born on Fridayday and your Akan name is" Kofi)
+// } else if (d === saturday && gender === male) {
+//   console.log("you were born on Saturday and your Akan name is" Kwame)
+// } else {
+//   console.log(Sorry dude!)
+// }
+// if (d === sunday && gender === female) {
+//   console.log("you were born on Sunday and your Akan name is" Akosua)
+// } else if (d === monday && gender === female) {
+//   console.log("you were born on Monday and your Akan name is "Adwoa)
+// } else if (d === tuesday && gender === female) {
+//   console.log(you were born on Tuesday and your Akan name is Abenaa)
+// } else if (d === wednesday && gender === female) {
+//   console.log(""you were born on Wednesday and your Akan name is "Akua)
+// } else if (d === thursday && gender === female) {
+//   console.log("you were born on Thursday and your Akan name is "Yaa)
+// } else if (d === friday && gender === female) {
+//   console.log("you were born on Friday and your Akan name is "Afua)
+// } else if (d === saturday && gender === female) {
+//   console.log("you were born on Saturday and your Akan name is "Ama)
+// } else {
+//   console.log("Sorry!Lady.")
