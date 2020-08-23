f0de5613496572e251a849bdad4068120a245946
Hassan Juma <okothhassanjuma@gmail.com>
2020-08-23 17:55:25 +0300

    adjustments made on functions

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,25 @@ function useInput() {
   var akanMen = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
   var akanWomen = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 
-  var CC = parseInt(bdate.substr(0, 2))
-  var YY = parseInt(bdate.substr(1, 3))
-  var MM = parseInt(bdate.substr(5, 6))
-  var DD = parseInt(bdate.substr(8, 9))
-  var d = parseInt(((CC / 4 - 2 * CC - 1) + (5 * YY / 4) + (26 * (MM + 1) / 10) + DD) % 7)
   
+  
+
   if (gender === "Male") {
     alert("Your Akan name is " + akanMen[d])
   } else
     alert("Your Akan name is " + akanWomen[d])
+  
+  
 }
+
+
+// const birthday = new Date('August 19, 1975 23:15:30');
+// const day1 = birthday.getDay();
+// // Sunday - Saturday : 0 - 6
+
+// console.log(day1);
+// expected output: 2
+
 
 
 // d = [0,1,2,3,4,5,6]
